9c073eb27d9a16b7f3c8190bdea21d2496c38459
sylvain joly <sylvain.joly00@orange.fr>
2020-05-13 15:12:04 +0200

    creation offer

--- screens/home.screen.js
+++ screens/home.screen.js
@@ -1,12 +1,14 @@
 import React from "react";
 
-import {Text, View, ScrollView} from "react-native";
+import {Text, View, ScrollView, ToastAndroid} from "react-native";
 import {Appbar, Button, TextInput, Divider, Checkbox, HelperText} from 'react-native-paper';
 import DatePicker from 'react-native-datepicker'
 import {AsyncStorage} from 'react-native';
+import axios from 'axios';
 
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {isLogged} from "./authCheck";
+import * as config from "../api/config";
 
 function HomeScreen({route, navigation}) {
 
@@ -69,7 +71,7 @@ function HomeScreen({route, navigation}) {
         const [descriptionCompanyErrorMsg, onDescriptionCompanyChangeErrorMsg] = React.useState("");
 
 
-        const [startAt, onStartAtChange] = React.useState("");
+        const [startAt, onStartAtChange] = React.useState(today());
         const [startAtError, onstartAtError] = React.useState(false);
         const [startAtErrorMsg, onstartAtErrorMsg] = React.useState("");
 
@@ -77,13 +79,145 @@ function HomeScreen({route, navigation}) {
         const [workingPlaceError, onWorkingPlaceError] = React.useState(false);
         const [workingPlaceErrorErrorMsg, onWorkingPlaceErrorChangeErrorMsg] = React.useState("");
 
+        const [contract, onChangeContract] = React.useState("");
+        const [contractError, onChangeContractError] = React.useState(false);
+        const [contractErrorMsg, onChangeContractErrorMsg] = React.useState("");
+
+        const [btnDisabled, onSetBtnDisabled] = React.useState(false);
+
+        const [date, setDate] = React.useState(today())
+
+
+        function today(){
+            var today = new Date();
+            var dd = today.getDate();
+
+            var mm = today.getMonth()+1;
+            var yyyy = today.getFullYear();
+            if(dd<10)
+            {
+                dd='0'+dd;
+            }
+
+            if(mm<10)
+            {
+                mm='0'+mm;
+            }
+
+            return today = yyyy+'-'+mm+'-'+dd;
+
+        }
 
         const onSubmit = async () => {
+            onSetBtnDisabled(true);
+
             const payload = {
-                name: name
+                name: name,
+                description: description,
+                companyDescription: descriptionCompany,
+                startAt: startAt,
+                workingPlace: workingPlace,
+                contract: contract
             };
 
-            let isReady = true
+            let isReady = true;
+
+            if(!payload.name || payload.name.trim() === "") {
+                onNameChangeError(true);
+                onNameChangeErrorMsg("Nom invalide")
+                isReady = false;
+            } else {
+                onNameChangeError(false);
+                onNameChangeErrorMsg("")
+            }
+
+            if(!payload.description || payload.description.trim() === "") {
+                onDescriptionError(true);
+                onDescriptionChangeErrorMsg("Description offre invalide")
+                isReady = false;
+            } else {
+                onDescriptionError(false);
+                onDescriptionChangeErrorMsg("")
+            }
+
+            if(!payload.companyDescription || payload.companyDescription.trim() === "") {
+                onDescriptionCompanyError(true);
+                onDescriptionCompanyChangeErrorMsg("Description entreprise invalide")
+                isReady = false;
+            } else {
+                onDescriptionError(false);
+                onDescriptionCompanyChangeErrorMsg("")
+            }
+
+            if(!payload.contract || payload.contract.trim() === "") {
+                onChangeContractError(true);
+                onChangeContractErrorMsg("Type de contrat invalide")
+                isReady = false;
+            } else {
+                onChangeContractError(false);
+                onChangeContractErrorMsg("")
+            }
+
+            if(!payload.workingPlace || payload.workingPlace.trim() === "") {
+                onWorkingPlaceError(true);
+                onWorkingPlaceErrorChangeErrorMsg("Lieu de travail invalide")
+                isReady = false;
+            } else {
+                onWorkingPlaceError(false);
+                onWorkingPlaceErrorChangeErrorMsg("")
+            }
+
+            if(isReady === false) {
+                onSetBtnDisabled(false) // release
+            } else {
+                
+                try {
+                    const t = await AsyncStorage.getItem('jwt');
+                    if(t === null) {
+                        navigation.navigate('Acceuil')
+                    } else {
+                        const me = await axios.post(`${config.default.URL}/me`, {token: t});
+
+                        if(me.status === 200 || me.status === 201) {
+                            const id = me.data[5].id;
+
+                            payload["user"] = "/users/"+id; // IRI
+
+                            const resp = await axios.post(`${config.default.URL}/offres`,payload, {
+                                headers: {
+                                    'Authorization': `Bearer ${t}`,
+                                    'Content-type': 'application/json'
+                                }
+                            });
+
+                            if(resp.status === 200 || resp.status === 201) {
+                                navigation.navigate('Invitations');
+                            } else{
+                                onSetBtnDisabled(false); // release
+                                ToastAndroid.showWithGravity(
+                                    "Erreur réseaux, veuillez réessayer",
+                                    ToastAndroid.SHORT,
+                                    ToastAndroid.CENTER
+                                );
+                            }
+                        } else {
+                            onSetBtnDisabled(false); // release
+                            ToastAndroid.showWithGravity(
+                                "Erreur réseaux, veuillez réessayer",
+                                ToastAndroid.SHORT,
+                                ToastAndroid.CENTER
+                            );
+                        }
+                    }
+                } catch(e) {
+                    onSetBtnDisabled(false); // release
+                    ToastAndroid.showWithGravity(
+                        "Erreur réseaux, veuillez réessayer",
+                        ToastAndroid.SHORT,
+                        ToastAndroid.CENTER
+                    );
+                }
+            }
 
 
         };
@@ -115,7 +249,7 @@ function HomeScreen({route, navigation}) {
                         >{nameErrorMsg}</HelperText>
 
                         <TextInput style={{padding: 5, margin: 10}}
-                                   label='Description'
+                                   label='Description offre'
                                    value={description}
                                    mode={'outlined'}
                                    error={descriptionError}
@@ -126,17 +260,6 @@ function HomeScreen({route, navigation}) {
                             visible={true}
                         >{descriptionErrorMsg}</HelperText>
 
-                        <TextInput style={{padding: 5, margin: 10}}
-                                   label='Description entreprise'
-                                   value={descriptionCompany}
-                                   mode={'outlined'}
-                                   error={descriptionCompanyError}
-                                   onChangeText={descriptionCompanyValue => onDescriptionCompanyChange(descriptionCompanyValue)}
-                        />
-                        <HelperText
-                            type="error"
-                            visible={true}
-                        >{descriptionCompanyErrorMsg}</HelperText>
 
                         <TextInput style={{padding: 5, margin: 10}}
                                    label='Description entreprise'
@@ -151,7 +274,19 @@ function HomeScreen({route, navigation}) {
                         >{descriptionCompanyErrorMsg}</HelperText>
 
                         <TextInput style={{padding: 5, margin: 10}}
-                                   label='Emplacement'
+                                   label='Type de contrat'
+                                   value={contract}
+                                   mode={'outlined'}
+                                   error={contractError}
+                                   onChangeText={contractValue => onChangeContract(contractValue)}
+                        />
+                        <HelperText
+                            type="error"
+                            visible={true}
+                        >{contractErrorMsg}</HelperText>
+
+                        <TextInput style={{padding: 5, margin: 10}}
+                                   label='Lieu de travail'
                                    value={workingPlace}
                                    mode={'outlined'}
                                    error={workingPlaceError}
@@ -162,13 +297,11 @@ function HomeScreen({route, navigation}) {
                             visible={true}
                         >{workingPlaceErrorErrorMsg}</HelperText>
                         <DatePicker
-                            style={{width: 200}}
-                            date={"2016-05-15"}
+                            style={{width: 300}}
+                            date={date}
                             mode="date"
                             placeholder="select date"
                             format="YYYY-MM-DD"
-                            minDate="2016-05-01"
-                            maxDate="2016-06-01"
                             confirmBtnText="Confirm"
                             cancelBtnText="Cancel"
                             customStyles={{
@@ -187,6 +320,9 @@ function HomeScreen({route, navigation}) {
                             }}
                         />
 
+                        <Button style={{marginTop:15, marginBottom: 100}} mode="contained" onPress={() => onSubmit()} disabled={btnDisabled}>
+                            Confirmer
+                        </Button>
                     </View>
                 </ScrollView>
             </ScrollView>
